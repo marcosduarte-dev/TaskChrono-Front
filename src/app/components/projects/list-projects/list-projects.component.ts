d9c15db03bac9d08b0c3bad74665ff18f70d13ef
Marcos Duarte <pe.marcos30@gmail.com>
2023-12-30 18:23:23 -0300

    Creating loader

--- src/app/components/projects/list-projects/list-projects.component.ts
+++ src/app/components/projects/list-projects/list-projects.component.ts
@@ -17,6 +17,7 @@ export class ListProjectsComponent implements OnInit {
   list: ProjectModel[] = [];
 
   submitted: boolean = false;
+  loaded: boolean = false;
 
   selectedProject!: ProjectModel[] | null;
 
@@ -38,6 +39,7 @@ export class ListProjectsComponent implements OnInit {
   getProjects() {
     this.projectService.getProjects().subscribe((projects: ProjectModel[]) => {
       this.list = projects;
+      this.loaded = true;
     });
   }
 
