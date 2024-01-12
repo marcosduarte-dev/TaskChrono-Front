b2b6dcba5f9298b0c3592d96889ca1b39712179c
Marcos Duarte <pe.marcos30@gmail.com>
2024-01-12 09:58:44 -0300

    Add first layout and logic

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { timer } from 'rxjs';
+import { Subscription, timer } from 'rxjs';
 import { SharedModule } from '../../shared/shared.module';
 import { CommonModule } from '@angular/common';
 
@@ -11,7 +11,52 @@ import { CommonModule } from '@angular/common';
   styleUrl: './timer.component.css',
 })
 export class TimerComponent implements OnInit {
-  constructor() {}
+  subscription!: Subscription;
+  counter: number;
+  isPaused: boolean;
+  display: string;
+
+  constructor() {
+    this.counter = 0;
+    this.isPaused = false;
+    this.display = '00:00:00';
+  }
 
   ngOnInit(): void {}
+
+  startTimer() {
+    const source = timer(1000, 1000);
+    this.subscription = source.subscribe((val) => {
+      if (!this.isPaused) {
+        this.counter = val;
+        console.log(val);
+        this.display = this.transform(this.counter);
+      }
+    });
+  }
+
+  stopTimer() {
+    console.log(this.counter);
+    this.subscription.unsubscribe();
+  }
+
+  pauseTimer() {
+    this.isPaused = true;
+  }
+
+  resumeTimer() {
+    this.isPaused = false;
+  }
+
+  transform(value: number): string {
+    var sec_num = value;
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - hours * 3600) / 60);
+    var seconds = sec_num - hours * 3600 - minutes * 60;
+    const formattedHours = hours.toString().padStart(2, '0');
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+    const formattedSeconds = seconds.toString().padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  }
 }
